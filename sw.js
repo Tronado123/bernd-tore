3cf768ce6828572b632967778a3a14b4882c2731
Tronado123 <andredeckert123@gmail.com>
2026-08-20 12:41:28 +0200

    PWA-Cache für Top-10-Version aktualisieren

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="bernd-tore-v6";
+const CACHE="bernd-tore-v7";
 const ASSETS=["./","index.html","styles.css","app.js","manifest.webmanifest","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(
